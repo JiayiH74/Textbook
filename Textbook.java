d649cf22eb0d31d691c67d307c1970f534174687
JiayiH74 <jiayih12@nycstudents.net>
2025-03-26 15:28:15 +0000

    final

--- Textbook.java
+++ Textbook.java
@@ -1,3 +1,29 @@
 public class Textbook extends Book
   {
+    private int edition;
+
+    public int getEdition()
+    {
+      return edition;
+    }
+
+    public Textbook(String title, double price, int edition)
+    {
+      super(title, price);
+      this.edition = edition;
+    }
+
+    public boolean canSubstituteFor(Textbook other)
+    {
+      if(getTitle().equals(other.getTitle()))
+      {
+        if(edition >= other.edition) return true;
+      }
+      return false;
+    }
+
+    public String getBookInfo()
+    {
+      return super.getBookInfo() + "-" + edition;
+    }
   }
